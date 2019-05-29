757746c85c1f5cf13ebbd5fe3a71088c44f6cb2f
Nabeelah <nabeelahyousuph@gmail.com>
2019-05-29 13:23:12 +0100

    Create todo list component
    - iterates over the todo data array and returns a list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component {
+
+const todoData = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  },
+  {
+    task: 'Do Assignment',
+    id: 123653098553,
+    completed: false
+  }
+]
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList: todoData
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,15 @@
+import React from 'react';
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+function TodoList(props) {
+    let { lists } = props;
+    return (
+        lists.map(list => {
+            return <div>{list}</div>
+        })
+    )
+}
+
+export default TodoList;
